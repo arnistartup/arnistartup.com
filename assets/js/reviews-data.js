060c157320c8a7c6d77afa7127ef2dc16f034304
arnistartup <arni.startup@gmail.com>
2026-08-28 14:08:18 -0400

    Publish review photos and review data.

--- assets/js/reviews-data.js
+++ assets/js/reviews-data.js
@@ -1,1 +1,9 @@
-window.ARNI_REVIEWS_SEED = [];
+window.ARNI_REVIEWS_SEED = [
+  {
+    "id": "pending-1787940480007",
+    "name": "Harika",
+    "text": "I loved the work!",
+    "src": "assets/reviews/review-1787940496723-harika.jpg",
+    "date": "2026-08-28"
+  }
+];
